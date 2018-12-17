d31b4684f315a69d36505ec53535417c96b964aa
Jason Moth <no@email.available>
2018-12-17 03:26:38 +0100

    heightened pow phase for testing

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -246,7 +246,7 @@ public:
         nMinerThreads = 0;
         nTargetTimespan = 1 * 60; // Jasmine: 1 day
         nTargetSpacing = 1 * 60;  // Jasmine: 1 minute
-        nLastPOWBlock = 200;
+        nLastPOWBlock = 1640;
         nMaturity = 15;
         nMasternodeCountDrift = 4;
         nModifierUpdateBlock = 51197; //approx Mon, 17 Apr 2017 04:00:00 GMT
